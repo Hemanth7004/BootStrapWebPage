85389a19a0235c82172ced93b70d9b161b7f5136
Hemanth7004 <71027967+Hemanth7004@users.noreply.github.com>
2020-09-13 19:33:07 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'view-employee/:id',
+    loadChildren: () => import('./view-employee/view-employee.module').then( m => m.ViewEmployeePageModule)
+  },
+  {
+    path: 'view-employee',
+    loadChildren: () => import('./view-employee/view-employee.module').then( m => m.ViewEmployeePageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
